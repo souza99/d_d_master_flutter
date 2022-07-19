cef9dc889ce84bd66c1c738e7a8415f503ceea39
souza99 <jpfsouza99@gmail.com>
2022-07-18 22:41:18 -0300

    inicio do salvamento de usuario

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializaApp();
+  await Firebase.initializeApp();
 
   runApp(MyApp());
 }
