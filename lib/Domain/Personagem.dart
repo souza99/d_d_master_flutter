65ae57875aa047c2a3d98f6da0f9244b5e4bf747
souza99 <jpfsouza99@gmail.com>
2022-07-19 21:47:25 -0300

    Cadastro personagem

--- lib/Domain/Personagem.dart
+++ lib/Domain/Personagem.dart
@@ -1,11 +1,44 @@
+import 'dart:ffi';
+
 import 'Classe.dart';
 import 'Item.dart';
 
 class Personagem {
-  late int id;
-  late String nome;
-  late int nivel;
-  late double vida;
-  late List<Item> iventario;
-  late Classe classe;
+  late int _id;
+  late String _nome;
+  late int _nivel;
+  late double _vida;
+  late List<Item> _iventario;
+  late Classe _classe;
+
+  int get id => _id;
+
+  set id(int id) {
+    _id = id;
+  }
+
+  String get nome => _nome;
+
+  set nome(String nome) {
+    _nome = nome;
+  }
+
+  int get nivel => _nivel;
+
+  set nivel(int nivel) {
+    _nivel = nivel;
+  }
+
+  double vida => _vida;
+
+  set vida(double vida){
+    _vida = vida;
+  }
+
+  List<Item> get item => _iventario;
+
+  set item(List<Item> item){
+    _iventario = item;
+  }
+
 }
---
+++ lib/View/personagem_cadastro.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../Domain/Classe.dart';
+
+class UsuarioCadastro extends StatelessWidget {
+  UsuarioCadastro({Key? key}) : super(key: key);
+
+  late int? id;
+  late String nome;
+  late int? nivel;
+  late double? vida;
+  //late List<Item> iventario;
+  late int? classe_id;
+
+  Future<int> salvarUsuario(
+      String nome, int nivel, double vida, Classe classe_id,
+      [int? id]) async {
+    String caminho = join(await getDatabasesPath(), 'banco');
+    Database banco = await openDatabase(caminho, version: 1);
+
+    String sql;
+    Future<int> linhasAfetadas;
+    if (id == null) {
+      sql =
+          'INSERT INTO personagem (nome, nivel, vida, classe_id) VALUES (?,?,?,?)';
+      linhasAfetadas = banco.rawInsert(sql, [
+        nome,
+        nivel,
+        vida,
+      ]);
+    } else {
+      sql =
+          'UPDATE personagem SET nome = ?, nivel = ?, vida = ?, classe_id = ? WHERE id = ?';
+      linhasAfetadas = banco.rawUpdate(sql, [nome, nivel, vida, classe_id, id]);
+    }
+    return linhasAfetadas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Cadastro de Personagem",
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.green,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  onChanged: (value) => nome = value,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    label: Text("Nome:"),
+                    hintText: "Ex: Cavaleiro de Dragão fenHur",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  onChanged: (value) => nivel = int.tryParse(value),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text("Nivel: "),
+                    hintText: "Ex: 18",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  onChanged: (value) => vida = double.tryParse(value),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    label: Text("Vida:"),
+                    hintText: "Ex: 200",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: TextFormField(
+                  onChanged: (value) => classe_id = int.tryParse(value),
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                    label: Text("Senha:"),
+                    hintText: "Ex: 123456",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: ElevatedButton(
+                  child: const Text("Salvar"),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                  ),
+                  onPressed: () {
+                    salvarUsuario(nome, telefone, email, senha, id);
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ));
+    throw UnimplementedError();
+  }
+}
