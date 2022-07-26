63040dedac4e1c84376f3e4ee859ae4fdfde5803
souza99 <jpfsouza99@gmail.com>
2022-07-25 22:34:01 -0300

    classe abilidade

--- lib/Domain/Classe.dart
+++ lib/Domain/Classe.dart
@@ -1,3 +1,11 @@
-class Classe{
-  
+import 'Habilidade.dart';
+
+class Classe {
+  late String _nome;
+  late List<Habilidade> _abilidades;
+  late double _ataque;
+  late double _defeza;
+  late double _vida;
+  late double _agilidade;
+  late double _mana;
 }
